5123d7d6c3eae56107e0f5f113e5b825e12e5e1c
zadvajr <danielzadva@gmail.com>
2024-08-10 21:38:51 +0100

    arrow functins

--- js-functions/index.js
+++ js-functions/index.js
@@ -35,3 +35,91 @@ let sum = function (a, b) {
 //To invoke or call it
 let output2 = document.getElementById("output2");
 output2.innerHTML += sum(2, 3); //5
+
+//The Function() Constructor - functions can also be defined with the builtin javascript function constructor called Function().
+//Syntax
+/**
+ * let functionName = new Function(parameters, code block);
+ */
+//Example
+let divide = new Function("a", "b", "return a / b");
+
+//To invoke or call it
+let output3 = document.getElementById("output3");
+output3.innerHTML += divide(10, 2); //5
+
+//Function Hoisting - JavaScript functions can be called before they are declared. This is because JavaScript function declarations are hoisted to the top of the containing function or script.
+//Functions defined with function expressions are not hoisted.
+//Example
+let output4 = document.getElementById("output4");
+output4.innerText += square(5); //25
+
+//Function Declaration
+function square(num) {
+  return num * num;
+}
+
+//Self-Invoking Functions - A self-invoking function is a function that runs as soon as it is defined.
+//Syntax
+/**
+ * (function() {
+ * code block
+ * })();
+ */
+//Example
+(function () {
+  document.getElementById("output5").innerText =
+    "This was added by self-invoking function";
+})();
+
+//using functions as value
+//js funtions can be used as value
+function add(a, b) {
+  return a + b;
+}
+
+let sumation = add(2, 3); //5
+document.getElementById("output6").innerText += sumation;
+
+//you can also use it in expression as follows
+let addition = add(7, 3) + 5; //15
+document.getElementById("output7").innerText += addition;
+
+//Functions are objects
+//In JavaScript, functions are first-class objects, which means they can be:
+//stored in a variable, object, or array
+//passed as an argument to a function
+//returned from a function
+//Example
+let multiply = function (a, b) {
+  return a * b;
+};
+
+let prod = multiply(2, 3); //6
+document.getElementById("output8").innerText += prod;
+
+//typeof function
+document.getElementById("output9").innerHTML += typeof multiply; //function
+
+//arguments object
+//JavaScript functions have a built-in object called the arguments object.
+function myFunction(a, b, c) {
+  return arguments.length;
+}
+
+document.getElementById("output9").innerHTML += "<br>" + myFunction(2, 3, 4); //3
+
+//toString() method - The toString() method returns the function as a string
+let str = multiply.toString();
+document.getElementById("output9").innerHTML +=
+  "<br> Funtion to String: " + str;
+
+//Arrow Functions - Arrow functions allow you to write shorter function syntax
+//You dont need to use the function keyword and return keyword in arrow functions
+//Syntax
+/**
+ * let functionName = (parameters) => expression
+ */
+//Example
+const cube = (num) => num * num * num;
+document.getElementById("output10").innerText += cube(3); //27
